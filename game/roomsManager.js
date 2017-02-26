74c4b5293c88731cd67c9504a195b8296e3d9baa
hukuuu <hukuuu@gmail.com>
2017-02-26 17:20:16 +0200

    comment test room

--- game/roomsManager.js
+++ game/roomsManager.js
@@ -1,18 +1,19 @@
 var Room = require('./room');
 var Player = require('./player');
 
-var testRoom = new Room(0);
-var testPlayer = new Player(1416486275063521, 'Николай', 0, 'RED_TELL')
-testRoom.join(testPlayer);
-testRoom.takePosition(testPlayer, 'RED_TELL');
-testRoom.game.redTell(testPlayer, {value: 'traktor', count: '4'});
-testRoom.takePosition(testPlayer, 'RED_GUESS');
+// var testRoom = new Room(0);
+// var testPlayer = new Player(1416486275063521, 'Николай', 0, 'RED_TELL')
+// testRoom.join(testPlayer);
+// testRoom.takePosition(testPlayer, 'RED_TELL');
+// testRoom.game.redTell(testPlayer, {value: 'traktor', count: '4'});
+// testRoom.takePosition(testPlayer, 'RED_GUESS');
 
 
 class RoomsManager {
   constructor() {
     this.id = 0;
-    this.rooms = [testRoom];
+    // this.rooms = [testRoom];
+    this.rooms = [];
   }
 
   createRoom() {
