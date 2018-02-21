616759bc91b0c039c9ba284da6874c1d0e770f1b
hukuuu <hukuuu@gmail.com>
2018-02-21 11:55:36 +0200

    bugfix: same red/blue ratio for every game
    every new game has the same amount red/blue cards ( should be random )
    fix: wrap the random call in a clojure

--- game/cardsGenerator.js
+++ game/cardsGenerator.js
@@ -1,7 +1,7 @@
 const shuffle = require('mout/array/shuffle');
 const words = require('./words.json').words
 
-const types = Array(8).fill('red').concat(Array(8).fill('blue')).concat(Array(7).fill('neutral'))
+const makeTypes = () => Array(8).fill('red').concat(Array(8).fill('blue')).concat(Array(7).fill('neutral'))
   .concat(Math.random() >= 0.5
     ? 'red'
     : 'blue')
@@ -12,7 +12,7 @@ const generate = () => {
   const shuffled = shuffle(words)
 
   let i = 0
-  const cards = types.map(type => ({
+  const cards = makeTypes().map(type => ({
     text: shuffled[i],
     type: type,
     pos: i++,
